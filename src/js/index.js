af063fb592732c3905d8cf9d8d7ef67ba8739ffb
Greet Van der Linden <vanderlindengreet@gmail.com>
2021-11-02 07:01:54 +0100

    local storage works for newsitems

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,34 @@ import { toggleNews } from "./data/news";
 import { toggleSong } from "./data/music";
 import { add, remove } from "./data/likes";
 
+// save to local storage
+
+const saveStore = () => {
+  window.localStorage.setItem("store", JSON.stringify(store.getState()));
+};
+const saveLikesStore = () => {
+  window.localStorage.setItem("likesStore", JSON.stringify(store.getState()));
+};
+
+store.subscribe(saveStore);
+likesStore.subscribe(saveLikesStore);
+
+// update UI when data has changed
+//  handle what happens after like state being toggled
+
+const likesChanged = (obj) => {
+  // if the card was liked
+  if (obj.liked) {
+    // make the star yellow
+    document.querySelector(`#${obj.id} .like`).classList.add("like--active");
+    // ik the card was unliked
+  } else {
+    // make the star white again
+    document.querySelector(`#${obj.id} .like`).classList.remove("like--active");
+  }
+};
+
+// Change data when left card is clicked
 // Handle left side cards being clicked
 
 const handleCardsClick = (event, className, reducer, action) => {
@@ -20,17 +48,15 @@ const handleCardsClick = (event, className, reducer, action) => {
     store.dispatch(action(targetObject));
     // if the card was liked
     if (targetObject.liked) {
-      // make the star yellow
-      event.target.classList.add("like--active");
       // add the card to the likesStore
       likesStore.dispatch(add(targetObject));
       // ik the card was unliked
     } else {
-      // make the star white again
-      event.target.classList.remove("like--active");
       // remove the card from the likesStore
       likesStore.dispatch(remove(targetObject));
     }
+    //  handle what happens after like state being toggled
+    likesChanged(targetObject);
   }
 };
 
@@ -49,6 +75,7 @@ document.querySelectorAll(".like").forEach((card) => {
 renderLikes = () => {
   document.querySelector(".likes__main").innerHTML = "";
   likesStore.getState().forEach((obj) => {
+    // console.log(obj);
     obj.render(obj.likesHolder);
 
     document
@@ -75,8 +102,8 @@ const handleLikedCardsClick = (event, className, reducer, action) => {
     store.dispatch(action(targetObject));
     // remove the card from the likesStore
     likesStore.dispatch(remove(targetObject));
-    // make the star white again
-    targetObject.star.classList.remove("like--active");
+    //  handle what happens after like state being toggled
+    likesChanged(targetObject);
   }
 };
 
@@ -100,3 +127,30 @@ document.querySelector(".heart").addEventListener("click", (event) => {
     document.querySelector("#root > main").style.display = "block";
   }
 });
+
+// Handle local storage for left cards
+
+const lsStore = JSON.parse(window.localStorage.getItem("store"));
+
+const reduxStore = store.getState();
+
+const sync = () => {
+  lsStore.newsReducer.forEach((object) => {
+    if (object.liked) {
+      const targetObject = reduxStore.newsReducer.filter(
+        (obj) => obj.name === object.name
+      )[0];
+      console.log(targetObject);
+      // toggle whether it was liked
+      store.dispatch(toggleNews(targetObject));
+
+      // add the card to the likesStore
+      likesStore.dispatch(add(targetObject));
+
+      //  handle what happens after like state being toggled
+      likesChanged(targetObject);
+    }
+  });
+};
+
+sync();
